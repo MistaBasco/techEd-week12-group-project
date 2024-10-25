aef6621124c62ca31bcd7f99768c3541d7bb6b2d
Jonathan Lee <kutsu89@gmail.com>
2024-10-25 10:32:19 +0100

    added comment and like components

---
+++ src/components/CommentFormInput.tsx
@@ -0,0 +1,17 @@
+export default function CommentFormInput({
+    onChange,
+    name,
+    placeholder,
+    type,
+  }) {
+    return (
+      <input
+        name={name}
+        placeholder={placeholder}
+        onChange={onChange}
+        type={type}
+        className="p-4 rounded-md hover:shadow-lg hover:shadow-black"
+      />
+    );
+  }
+  
