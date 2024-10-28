513d2542426a046b7b5b1793788a75e1d3f5bce7
George Croucher <gpcroucher@gmail.com>
2024-10-28 14:10:09 +0000

    fixed issue with navbar while signed out

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,20 @@
+import { SignedIn } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 import Link from "next/link";
 
 export default async function NavBar() {
-  const user = await currentUser();
-  const username = user?.username; // TODO use context when available
+  async function getUsername() {
+    const user = await currentUser();
+    return user?.username; // TODO use context when available
+  }
+
   return (
     <nav className="flex h-full items-center justify-center gap-4 pl-4">
       <Link href="/">Home</Link>
       <Link href="/feed">Feed</Link>
-      {username && <Link href={`/${username}`}>My Profile</Link>}
+      <SignedIn>
+        <Link href={`/${await getUsername()}`}>My Profile</Link>
+      </SignedIn>
     </nav>
   );
 }
