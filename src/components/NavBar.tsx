60c995f50a553d4b3e858f7737f6d2f6e2a608c9
George Croucher <gpcroucher@gmail.com>
2024-10-30 17:20:35 +0000

    changed profile link in navbar to point to current user profile page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,21 @@
+import { getUserIdByClerkId } from "@/utilities/getUserByClerkId";
 import { SignedIn } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 import Link from "next/link";
 
-
 export default async function NavBar() {
-  async function getUsername() {
-    const user = await currentUser();
-    return user?.username; // TODO use context when available
+  async function getUserId() {
+    const myUser = await currentUser();
+    return myUser ? await getUserIdByClerkId(myUser!.id) : null;
   }
 
   return (
     <nav className="flex h-full items-center justify-center gap-4 pl-4">
       <Link href="/">Home</Link>
       <Link href="/feed">Feed</Link>
-  
+
       <SignedIn>
-        <Link href={`/${await getUsername()}`}>My Profile</Link>
+        <Link href={`/${await getUserId()}`}>My Profile</Link>
         {/* TODO change this link when we make the profile page */}
       </SignedIn>
     </nav>
