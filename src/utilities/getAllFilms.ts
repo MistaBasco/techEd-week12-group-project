a257770d23a2f0290fce510627464bcc26fdd1ee
George Croucher <gpcroucher@gmail.com>
2024-10-31 13:43:42 +0000

    fixes for /films and improvements to getAllFilms utility

--- src/utilities/getAllFilms.ts
+++ src/utilities/getAllFilms.ts
@@ -1,10 +1,18 @@
 import { Film } from "./getFilmById";
 import connect from "./connect";
 
-export default async function getAllFilms(): Promise<Film[]> {
+export default async function getAllFilms(
+  limit: number,
+  sortDirection: "asc" | "desc"
+): Promise<Film[]> {
+  if (sortDirection !== "asc" && sortDirection !== "desc") {
+    throw new Error("Not a valid sort direction for getAllFilms.");
+  }
+
   const db = connect();
   const result = await db.query<Film>(
-    `SELECT * FROM films ORDER BY film_id ASC`
+    `SELECT * FROM films ORDER BY film_id ${sortDirection} LIMIT $1`,
+    [limit]
   );
   return result.rows;
 }
