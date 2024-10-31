24b2e47a2196135fd68549afc71d6ed5e04f7a4a
Jonathan Lee <kutsu89@gmail.com>
2024-10-31 09:21:32 +0000

    added useRouter to commentForm for refresh upon comment submit

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import CommentFormInput from "./CommentFormInput";
 import { Button } from "@chakra-ui/react";
+import { useRouter } from "next/navigation"; // Import useRouter
 
 export default function PostComment({
   submitComment,
@@ -14,12 +15,12 @@ export default function PostComment({
     username: "",
     comment: "",
   });
-
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const router = useRouter(); // Initialize useRouter
 
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-
     setIsSubmitting(true);
 
     const commentData = new FormData(event.currentTarget);
@@ -32,6 +33,9 @@ export default function PostComment({
 
       setFormData({ username: "", comment: "" });
       event.currentTarget.reset();
+
+      // Refresh the page
+      router.refresh();
     } catch (error) {
       console.error("Error posting comment:", error);
     } finally {
@@ -47,31 +51,28 @@ export default function PostComment({
   }
 
   return (
-    <>
-      <form
-        method="POST"
-        onSubmit={handleSubmit}
-        className="flex flex-col bg-white p-6 rounded-lg shadow-md space-y-4"
+    <form
+      method="POST"
+      onSubmit={handleSubmit}
+      className="flex flex-col bg-white p-6 rounded-lg shadow-md space-y-4"
+    >
+      <CommentFormInput
+        onChange={handleChange}
+        placeholder="Enter your comment..."
+        value={formData.comment}
+      />
+      <Button
+        type="submit"
+        colorScheme="blue"
+        size="md"
+        fontWeight="bold"
+        w="full"
+        _hover={{ bg: "blue.600", transform: "scale(1.03)" }}
+        transition="transform 0.2s ease-in-out, background-color 0.2s"
+        disabled={formData.comment.length < 1}
       >
-        <CommentFormInput
-          onChange={handleChange}
-          placeholder="Enter your comment..."
-          value={formData.comment}
-        />
-        <Button
-          type="submit"
-          colorScheme="blue"
-          size="md"
-          fontWeight="bold"
-          w="full"
-          _hover={{ bg: "blue.600", transform: "scale(1.03)" }}
-          transition="transform 0.2s ease-in-out, background-color 0.2s"
-          // isLoading={isSubmitting}
-          disabled={formData.comment.length < 1}
-        >
-          {isSubmitting ? "Submitting..." : "Submit"}
-        </Button>
-      </form>
-    </>
+        {isSubmitting ? "Submitting..." : "Submit"}
+      </Button>
+    </form>
   );
 }
